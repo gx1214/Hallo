c03eb2ede8052e623ca948a76c34c1b3c9e58f64
Administrator <Administrator@PC-20181011UYCV>
2019-02-26 12:31:07 +0800

    2.0.0

--- Hello/src/cn/oracle/Hello.java
+++ Hello/src/cn/oracle/Hello.java
@@ -6,23 +6,23 @@ public class Hello {
 		int times=1;								//记录输入的次数
 		Random random =new Random();				//定义随机数对象
 		int ran= random.nextInt(100)+1; 			//System.out.println(ran); 生成随机数
-		System.out.println("请输入");				//控制台输入	
+		System.out.println("输入");				    //控制台输入	
 		Scanner sc=new Scanner(System.in);			//获取用户输入数据的方法
 		while(true){								//while循环
 			System.out.println("plz enter some number");
 			String num=sc.nextLine();				//获取用户输入的数据
 			int _num=Integer.parseInt(num);			//将获取的数据转换成int型变量
 			if(_num==ran){
-				System.out.println("输入正确"+ran+"共输入了"+times+"次");
+				System.out.println("输入正确"+ran+"共输入"+times+"次");
 				break;	
 			}
 			else{
 				times++;
 				if(_num>ran){
-					System.out.println("太大了");
+					System.out.println("大了");
 				}
 				else{
-					System.out.println("太小了");
+					System.out.println("小了");
 				}
 			}
 		}
